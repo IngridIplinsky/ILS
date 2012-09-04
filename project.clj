d001c111c6c24df1f4daeffdbfe3f616528bce82
Pablo Henrique <pablo.haoko@gmail.com>
2012-09-04 13:16:04 -0300

    Modificacoes no project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject ils "0.1.0-SNAPSHOT"
+(defproject tanara "0.1.0-SNAPSHOT"
             :description "ILS: Sistema Tutor Inteligente"
             :dependencies [[org.clojure/clojure "1.3.0"]
                            [noir "1.2.1"]
                            [org.clojure/java.jdbc "0.1.3"]
                            [com.h2database/h2 "1.3.160"]]
-            :main ils.server)
+            :main tanara.server)
 
