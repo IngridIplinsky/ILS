5ce9761a1dee4711ce90ea9281a893d4ae70ae19
Pablo Henrique <pablo.haoko@gmail.com>
2012-09-14 11:25:09 -0300

    Arquivos do DB e projeto.

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject tanara "0.1.0-SNAPSHOT"
-            :description "ILS: Sistema Tutor Inteligente"
+(defproject ils "0.1.0-SNAPSHOT"
+            :description "FIXME: write description"
             :dependencies [[org.clojure/clojure "1.3.0"]
                            [noir "1.2.1"]
                            [org.clojure/java.jdbc "0.1.3"]
-                           [com.h2database/h2 "1.3.160"]]
-            :main tanara.server)
+                           [com.h2database/h2 "1.3.160"]
+                           [org.clojure/clojure-contrib "1.2.0"]]
+            :main ils.server)
 
