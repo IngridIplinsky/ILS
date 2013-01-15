84272e077e93a9c44969f2de65bc1d637dfb08c0
eduardo <eduardo@eduardo-Inspiron-1440.(none)>
2013-01-15 19:58:50 -0200

    EBayes adicionada a biblioteca

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
 (defproject ils "0.1.0-SNAPSHOT"
             :description "FIXME: write description"
-            :dependencies [[org.clojure/clojure "1.3.0"]
-                           [noir "1.2.1"]
-			   [org.clojure/data.xml "0.0.6"]	
-                           [org.clojure/java.jdbc "0.1.3"]
-                           [com.h2database/h2 "1.3.160"]
-                           [org.clojure/clojure-contrib "1.2.0"]]
+            :dependendencies [[org.clojure/clojure "1.4.0"]
+		                      [noir "1.2.1"]
+				   			  [org.clojure/data.xml "0.0.6"]	
+		                      [org.clojure/java.jdbc "0.1.3"]
+		                      [com.h2database/h2 "1.3.160"]
+		                      [org.clojure/clojure-contrib "1.2.0"]]
             :main ils.server)
 
