42c39cb964b1b6befc01a3924f98f8a8d46e6023
pablohenrique <pablo.haoko@gmail.com>
2013-01-19 08:19:56 -0200

    snapshot

--- src/ils/models/pedagogico/pedagogicox.clj
+++ src/ils/models/pedagogico/pedagogicox.clj
@@ -1,7 +1,6 @@
 (ns ils.models.pedagogico.pedagogicox
 	(:use 
 		[ils.models.pedagogico.main]
-		[ils.models.pedagogico.formatapergunta]
 		[ils.models.pedagogico.compilador]
 		[ils.models.pedagogico.corretor]
 	)
